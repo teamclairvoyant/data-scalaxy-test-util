76d9f8d5f45b5ef25edbf1442a6d302f2fb45322
rahulbhatia023 <rahul.bhatia023@gmail.com>
2023-08-18 16:03:19 -0400

    Added utility methods for parsing csv files and csv text to spark dataframes

--- src/main/scala/com/clairvoyant/data/scalaxy/test/util/DataScalaxyTestUtil.scala
+++ src/main/scala/com/clairvoyant/data/scalaxy/test/util/DataScalaxyTestUtil.scala
@@ -1,7 +1,6 @@
 package com.clairvoyant.data.scalaxy.test.util
 
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -20,5 +19,19 @@ trait DataScalaxyTestUtil extends AnyFlatSpec with Matchers with DataFrameMatche
       .option("multiline", value = true)
       .option("inferSchema", value = true)
       .json(Seq(text).toDS())
+
+  def readCSVFromText(text: String, csvOptions: Map[String, String] = Map.empty): DataFrame =
+    sparkSession.read
+      .options(csvOptions)
+      .csv {
+        Seq(text)
+          .flatMap(_.split(csvOptions.getOrElse("lineSep", "\n")))
+          .toDS()
+      }
+
+  def readCSVFromFile(path: String, csvOptions: Map[String, String] = Map.empty): DataFrame =
+    sparkSession.read
+      .options(csvOptions)
+      .csv(path)
 
 }
